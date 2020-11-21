a60d23f7dd7cb4765497d9f4b6055b527480bfec
Jason Snow <jsn.snw@gmail.com>
2020-11-21 13:53:43 -0500

    Add ability to set install path. Resolves #7

--- jdk/__init__.py
+++ jdk/__init__.py
@@ -12,49 +12,50 @@ from urllib import request
 from sys import platform, maxsize
 from os import path
 
-_IS_WINDOWS = os.name == 'nt'
-_IS_DARWIN = platform == 'darwin'
-_UNPACK200 = 'unpack200.exe' if _IS_WINDOWS else 'unpack200'
-_UNPACK200_ARGS = '-r -v -l ""' if _IS_WINDOWS else ''
-_USER_DIR = path.expanduser('~')
-_JRE_DIR = path.join(_USER_DIR, '.jre')
-_JDK_DIR = path.join(_USER_DIR, '.jdk')
+_IS_WINDOWS = os.name == "nt"
+_IS_DARWIN = platform == "darwin"
+_UNPACK200 = "unpack200.exe" if _IS_WINDOWS else "unpack200"
+_UNPACK200_ARGS = '-r -v -l ""' if _IS_WINDOWS else ""
+_USER_DIR = path.expanduser("~")
+_JRE_DIR = path.join(_USER_DIR, ".jre")
+_JDK_DIR = path.join(_USER_DIR, ".jdk")
 
-OS = 'windows' if _IS_WINDOWS else 'mac' if _IS_DARWIN else platform
-ARCH = 'x64' if maxsize > 2**32 else 'x32'
+OS = "windows" if _IS_WINDOWS else "mac" if _IS_DARWIN else platform
+ARCH = "x64" if maxsize > 2 ** 32 else "x32"
 
-_Path = namedtuple('_Path', 'dir base name ext')
+_Path = namedtuple("_Path", "dir base name ext")
 
-_TAR = '.tar'
-_TAR_GZ = '.tar.gz'
-_ZIP = '.zip'
-_SEVEN_ZIP = '.7z'
+_TAR = ".tar"
+_TAR_GZ = ".tar.gz"
+_ZIP = ".zip"
+_SEVEN_ZIP = ".7z"
 
 
 class Implementation:
 
-    OPENJ9 = 'openj9'
-    HOTSPOT = 'hotspot'
+    OPENJ9 = "openj9"
+    HOTSPOT = "hotspot"
 
 
 def normalize_version(version: str) -> str:
-    if version == '1.8':
-        return '8'
+    if version == "1.8":
+        return "8"
     return version
 
 
 def get_download_url(
-        version: str,
-        operating_system: str = OS,
-        arch: str = ARCH,
-        impl: str = Implementation.HOTSPOT,
-        jre: bool = False) -> str:
+    version: str,
+    operating_system: str = OS,
+    arch: str = ARCH,
+    impl: str = Implementation.HOTSPOT,
+    jre: bool = False,
+) -> str:
 
     version = normalize_version(version)
     if jre:
-        return f'https://api.adoptopenjdk.net/v3/binary/latest/{version}/ga/{operating_system}/{arch}/jre/{impl}/normal/adoptopenjdk'
+        return f"https://api.adoptopenjdk.net/v3/binary/latest/{version}/ga/{operating_system}/{arch}/jre/{impl}/normal/adoptopenjdk"
     else:
-        return f'https://api.adoptopenjdk.net/v3/binary/latest/{version}/ga/{operating_system}/{arch}/jdk/{impl}/normal/adoptopenjdk'
+        return f"https://api.adoptopenjdk.net/v3/binary/latest/{version}/ga/{operating_system}/{arch}/jdk/{impl}/normal/adoptopenjdk"
 
 
 def _get_normalized_compressed_file_ext(file: str) -> str:
@@ -68,18 +69,15 @@ def _get_normalized_compressed_file_ext(file: str) -> str:
         return _SEVEN_ZIP
 
 
-def _extract_files(
-        file: str,
-        file_ending: str,
-        destination_folder: str) -> str:
+def _extract_files(file: str, file_ending: str, destination_folder: str) -> str:
     if path.isfile(file):
         start_listing = set(os.listdir(destination_folder))
 
         if file_ending == _TAR:
-            with tarfile.open(file, 'r:') as tar:
+            with tarfile.open(file, "r:") as tar:
                 tar.extractall(path=destination_folder)
         elif file_ending == _TAR_GZ:
-            with tarfile.open(file, 'r:gz') as tar:
+            with tarfile.open(file, "r:gz") as tar:
                 tar.extractall(path=destination_folder)
         elif file_ending == _ZIP:
             with zipfile.ZipFile(file) as z:
@@ -109,30 +107,24 @@ def _unpack_jars(fs_path: str, java_bin_path: str) -> None:
                 _unpack_jars(current_path, java_bin_path)
         else:
             file_name, file_ext = path.splitext(fs_path)
-            if file_ext.endswith('pack'):
+            if file_ext.endswith("pack"):
                 p = _path_parse(fs_path)
                 name = path.join(p.dir, p.name)
                 tool_path = path.join(java_bin_path, _UNPACK200)
-                run([tool_path, _UNPACK200_ARGS,
-                     f'{name}.pack', f'{name}.jar'])
+                run([tool_path, _UNPACK200_ARGS, f"{name}.pack", f"{name}.jar"])
 
 
 def _decompress_archive(
-        repo_root: str,
-        file_ending: str,
-        destination_folder: str) -> str:
+    repo_root: str, file_ending: str, destination_folder: str
+) -> str:
     if not path.exists(destination_folder):
         os.mkdir(destination_folder)
 
     jdk_file = path.normpath(repo_root)
 
     if path.isfile(jdk_file):
-        jdk_directory = _extract_files(
-            jdk_file,
-            file_ending,
-            destination_folder
-        )
-        jdk_bin = path.join(jdk_directory, 'bin')
+        jdk_directory = _extract_files(jdk_file, file_ending, destination_folder)
+        jdk_bin = path.join(jdk_directory, "bin")
         _unpack_jars(jdk_directory, jdk_bin)
 
         return jdk_directory
@@ -141,39 +133,41 @@ def _decompress_archive(
 
 
 def _download(download_url):
-    req = request.Request(download_url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = request.Request(download_url, headers={"User-Agent": "Mozilla/5.0"})
 
     jdk_file = None
     with request.urlopen(req) as open_request:
         info = open_request.info()
-        if 'Content-Disposition' in info:
-            content_disposition = info['Content-Disposition']
+        if "Content-Disposition" in info:
+            content_disposition = info["Content-Disposition"]
             _, params = cgi.parse_header(content_disposition)
-            if 'filename' in params:
-                jdk_file = params['filename']
+            if "filename" in params:
+                jdk_file = params["filename"]
                 jdk_file = path.join(tempfile.gettempdir(), jdk_file)
 
-                with open(jdk_file, 'wb') as out_file:
+                with open(jdk_file, "wb") as out_file:
                     shutil.copyfileobj(open_request, out_file)
     return jdk_file
 
 
 def install(
-        version: str,
-        operating_system: str = OS,
-        arch: str = ARCH,
-        impl: str = Implementation.HOTSPOT,
-        jre: bool = False) -> str:
+    version: str,
+    operating_system: str = OS,
+    arch: str = ARCH,
+    impl: str = Implementation.HOTSPOT,
+    jre: bool = False,
+    path: str = None,
+) -> str:
     url = get_download_url(version, operating_system, arch, impl, jre)
+
+    if not path:
+        path = _JRE_DIR if jre else _JDK_DIR
 
     jdk_file = None
     try:
         jdk_file = _download(url)
         jdk_ext = _get_normalized_compressed_file_ext(jdk_file)
-        jdk_dir = _decompress_archive(
-            jdk_file,
-            jdk_ext, _JRE_DIR if jre else _JDK_DIR
-        )
+        jdk_dir = _decompress_archive(jdk_file, jdk_ext, path)
 
         return jdk_dir
     finally:
@@ -182,14 +176,12 @@ def install(
 
 
 def uninstall(version: str, jre: bool = False):
-    version = f'jdk{version}'
+    version = f"jdk{version}"
     if jre:
-        versions = (v for v in os.listdir(_JRE_DIR)
-                    if version in v.replace('-', ''))
+        versions = (v for v in os.listdir(_JRE_DIR) if version in v.replace("-", ""))
         for v in versions:
             shutil.rmtree(path.join(_JRE_DIR, v))
     else:
-        versions = (v for v in os.listdir(_JDK_DIR)
-                    if version in v.replace('-', ''))
+        versions = (v for v in os.listdir(_JDK_DIR) if version in v.replace("-", ""))
         for v in versions:
             shutil.rmtree(path.join(_JDK_DIR, v))
