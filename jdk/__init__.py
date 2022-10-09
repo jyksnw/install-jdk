7ab392ef8f184debd561e04d8f8422ddf3712e33
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 22:38:16 +0000

    Adding tarfile member sanitization to extractall()

--- jdk/__init__.py
+++ jdk/__init__.py
@@ -75,16 +75,73 @@ def _extract_files(file: str, file_ending: str, destination_folder: str) -> str:
 
         if file_ending == _TAR:
             with tarfile.open(file, "r:") as tar:
-                tar.extractall(path=destination_folder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=destination_folder)
         elif file_ending == _TAR_GZ:
             with tarfile.open(file, "r:gz") as tar:
-                tar.extractall(path=destination_folder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=destination_folder)
         elif file_ending == _ZIP:
             with zipfile.ZipFile(file) as z:
                 z.extractall(path=destination_folder)
         elif file_ending == _SEVEN_ZIP:
             with lzma.open(file) as z:
-                z.extractall(path=destination_folder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(z, path=destination_folder)
 
         end_listing = set(os.listdir(destination_folder))
         jdk_directory = next(iter(end_listing.difference(start_listing)))
