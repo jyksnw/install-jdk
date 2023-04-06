5c943337c9109408b4ba2b104d5ad63632c78d30
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-06 16:58:15 +0200

    modifies the noxfile 🚀

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,10 @@
 import nox
+import sys
 
-@nox.session
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"])
 def tests(session):
+    if sys.version_info < (3, 6):
+        session.skip("Tests require Python 3.6 or later.")
     session.install("-r", "requirements.txt")
     session.run('pytest')
